e8021303e9c2be833402394485d4df512be8b307
Bob Nelson <rwnelson86@gmail.com>
2023-05-02 17:23:02 -0400

    Tweaking css

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,20 +3,13 @@ import React from "react";
 const HomePage = (props) => {
     return(
         <div className="primary">
-
             <h2 className="secondary header">Welcome to the Homepage</h2>
-        
-        <div>
             <h4 className="secondary header">
                 Rate my salad!
             </h4>
-        </div>
-        <div>
             <p>
                 Developed by Jess, Bob, Todd, Tim
             </p>
-        </div>
-
         </div>
     )
 }
