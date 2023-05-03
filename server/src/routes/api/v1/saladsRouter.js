28e365ac0e92518463e275b845cf28b4e6fe5af2
Todd Garrison <toddlgarrison@gmail.com>
2023-05-03 14:29:25 -0400

    Cleanup saladsRouter and SaladTile

--- server/src/routes/api/v1/saladsRouter.js
+++ server/src/routes/api/v1/saladsRouter.js
@@ -3,7 +3,6 @@ import objection from "objection"
 
 import { Salad } from "../../../models/index.js"
 
-
 const saladsRouter = new express.Router()
 
 saladsRouter.get("/", async (req, res) => {
@@ -19,13 +18,12 @@ saladsRouter.get("/:id", async (req, res) => {
     const saladId = req.params.id
     
     try {
-        const showSalads = await Salad.query().findById(saladId)
+        const showSalad = await Salad.query().findById(saladId)
         return res.status(200).json({ salad: showSalads })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ errors: error })
     }
 })
-
 
 export default saladsRouter;
