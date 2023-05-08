09728451264d0ac249671723fd5f2e09b7d2059d
Todd Garrison <toddlgarrison@gmail.com>
2023-05-08 15:32:39 -0400

    Remove console.log

--- server/src/routes/api/v1/userActivityRouter.js
+++ server/src/routes/api/v1/userActivityRouter.js
@@ -21,7 +21,6 @@ userActivityRouter.get("/", async (req, res) => {
         user.salads = sortedSaladsWithUser
         return res.status(200).json({ user })
     } catch (error) {
-        console.log(error)
         return res.status(500).json({ errors: error })
     }
 })
