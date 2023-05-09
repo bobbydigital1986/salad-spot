8b180b98bee8208f7a2ec0e27c94fcca09062268
Bob Nelson <rwnelson86@gmail.com>
2023-05-09 12:14:51 -0400

    Added frontend components for voting

--- client/src/components/SaladList.js
+++ client/src/components/SaladList.js
@@ -4,6 +4,8 @@ import SaladTile from "./SaladTile"
 const SaladList = (props) => {
 
     const [salads, setSalads] = useState([])
+    const [votes, setVotes] = useState([])
+
     const getSalads = async() => {
         try{
             const response = await fetch("/api/v1/salads")
@@ -23,11 +25,50 @@ const SaladList = (props) => {
         getSalads()
     }, [])
 
+    const postVote = async({vote}) => {
+        console.log("hey there")
+        try {
+            const response = fetch("/api/v1/vote", {
+                method: "POST",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                }),
+                body: JSON.stringify({ vote })
+            })
+            if (!response.ok) {
+                if(response.status === 422) {
+                    const errorBody = await response.json()
+                    const newErrors = translateServerErrors(errorBody.errors.data)
+                    return setErrors(newErrors)
+                } else {
+                    const errorMessage = `${response.status} (${response.statusText})`
+                    const error = new Error(errorMessage)
+                    throw(error)
+                }
+            } else {
+                const body = await response.json()
+                const newSalads = salads.map(salad => {
+                    if (salad.id === body.vote.saladId) {
+                        salad.vote = body.vote.rating
+                        return salad
+                    }
+                    return salad
+                    
+                })
+                setSalads(newSalads)
+
+            }
+        } catch (error) {
+            console.log(error)
+        }    
+    }
+
     const saladItems = salads.map((salad)=> {
         return (
             <SaladTile
                 key={salad.id}
                 salad={salad}
+                postVote={postVote}
             />
         )
     })
--- client/src/components/SaladTile.js
+++ client/src/components/SaladTile.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 const SaladTile = (props) => {
-    const { name, description, id, user } = props.salad 
+    
+    const { name, description, id, user, vote } = props.salad 
+    const [voteStatus, setVoteStatus] = useState({
+        vote
+    })
+    const handleClick = (event) => { 
+        if (event.target === "upvote") {
+            props.postVote(1)
+        } else if (event.target === "downvote") {
+            props.postVote(-1)
+        }
+    }
+
+    let upVoteStyle = "gray"
+    let downVoteStyle = "gray"
+
+    if (voteStatus == -1) {
+        downVoteStyle = "orange"
+    } else if (voteStatus == 1) { 
+        upVoteStyle = "orange"
+    } else {
+
+    }
+
     
     return (
         <>
             <li><Link to={`/salads/${id}`}>{name}</Link></li>
+            <input className={`button ${upVoteStyle}`} name="upvote" onClick={handleClick} value="upvote"/>
+            <input className={`button ${downVoteStyle}`} name="downvote" onClick={handleClick} value="downvote"/>
             <p>{description}</p>
             <h4 className='submitted-by'>Submitted by {user}</h4>
         </>
